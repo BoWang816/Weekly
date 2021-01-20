bf00472b8b927416f4f3baac846fde9020ad4e46
恪晨 <bo.wang1016@outlook.com>
2021-01-20 13:26:43 +0800

    update gulp config and build script

--- gulpfile.js
+++ gulpfile.js
@@ -62,6 +62,19 @@ gulp.task('minify-images', async () => {
         .pipe(gulp.dest('./public/img'))
 })
 
+
+// 壓縮 public/uploads 目錄內圖片
+gulp.task('minify-images', async () => {
+    gulp.src('./public/img/**/*.*')
+        .pipe(imagemin({
+            optimizationLevel: 5, // 類型：Number  預設：3  取值範圍：0-7（優化等級）
+            progressive: true, // 類型：Boolean 預設：false 無失真壓縮jpg圖片
+            interlaced: false, // 類型：Boolean 預設：false 隔行掃描gif進行渲染
+            multipass: false // 類型：Boolean 預設：false 多次優化svg直到完全優化
+        }))
+        .pipe(gulp.dest('./public/img'))
+})
+
 // 執行 gulp 命令時執行的任務
 gulp.task('default', gulp.parallel(
     'compress', 'minify-css', 'minify-html', 'minify-images'
